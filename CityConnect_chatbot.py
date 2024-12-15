5449cb170f35026c352dddd4c3eb5803bac72179
Monika Grotowska <0grotm62@solent.ac.uk>
2024-12-15 18:29:53 +0000

    chatbot 1

---
+++ CityConnect_chatbot.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.keras')
+
+#Clean up the sentences
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+#Converts the sentences into a bag of words
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence) #bow: Bag Of Words, feed the data into the neural network
+    res = model.predict(np.array([bow]))[0] #res: result. [0] as index 0
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("""
+=============================================================
+         City Connect: Your Link to the Local Life! 
+=============================================================
+
+Welcome to City Connect! 
+This virtual assistant is here to provide you with:
+    - Comprehensive information about Southampton.
+    - Assistance with local services and city updates.
+    - Fun facts, history, and recreation tips about the city.
+
+Whether you're a resident, a visitor, or just curious, 
+City Connect is your ideal companion for all things Southampton.
+
+ Ask away and let us guide you through the city! 
+
+=============================================================
+""")
+
+while True:
+    message = input("Your question: ")
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
+
+
+# In[ ]:
+
+
+
+
